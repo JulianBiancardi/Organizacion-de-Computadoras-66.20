f15f44a6285a762eb235092035fd91cc299b149d
sebastian-outeiro <outeiro.sebastian@gmail.com>
2020-10-20 23:27:14 -0300

    Decode test fix

--- TP0/decode-test.c
+++ TP0/decode-test.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <string.h>
 #include "decode.h"
 
-void testDecode(char* input, char* expectedOutput);
+
+int testDecode(char* input, char* expectedOutput);
 
 int main(){
-	printf("Testing");
-	testDecode("TWFu", "Man");
-	testDecode("QZ==", "e");
+	printf("Start Testing Decode\n");
+	int success = testDecode("TWFu", "Man");
+	success = success & testDecode("ZQ==", "e");
+	success = success & testDecode("ZS4=", "e.");
+	if (success)
+		printf("Tests: OK\n");
+	else
+		printf("Tests: ERROR\n");
 }
 
-void testDecode(char* input, char* expectedOutput){
-	char actualOutput[3];
-	int sucess = decode(input, actualOutput);
+int testDecode(char* input, char* expectedOutput){
+	char actualOutput[3] = "";
+	int decodeResult = decode(input, actualOutput);
+	int expectedEqual = strcmp(actualOutput,expectedOutput);
 	printf("Encode text was: '%s'. Expected decode is: '%s'. Actual decode result: '%s'\n",input, expectedOutput, actualOutput);
-	//assert(actualOutput == expectedOutput);
-	assert(sucess == 0);
+	return (!decodeResult) & (!expectedEqual);
 }
+
