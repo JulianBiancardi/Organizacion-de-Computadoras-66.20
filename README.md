3925c2b95decf7dce85889474c28383d97ec7bcb
JulianBiancardi <JulianBiancardi@github.com>
2020-11-04 12:34:18 -0300

    Update

--- README.md
+++ README.md
@@ -8,14 +8,15 @@ Repositorio para la Materia Organización de Computadoras [6620]
 
 # Integrantes
 
-Alumno | Padrón
---|--
-**BIANCARDI**, Julián | _103945_
-**CZOP**, Santiago Nicolás | _104057_
-**OUTEIRO**, Sebastián | _92108_
+| Alumno                     | Padrón   |
+| -------------------------- | -------- |
+| **BIANCARDI**, Julián      | _103945_ |
+| **CZOP**, Santiago Nicolás | _104057_ |
+| **OUTEIRO**, Sebastián     | _92108_  |
 
 # Trabajos Prácticos
 
 * [TP0](https://github.com/JulianBiancardi/Organizasion-de-Computadoras-66.20/tree/main/TP0)
+* [TP1](https://github.com/JulianBiancardi/Organizasion-de-Computadoras-66.20/tree/main/TP1)
 
 
--- TP1/Euclides.c
+++ TP1/Euclides.c
@@ -14,9 +14,9 @@ unsigned int mcd_rec(unsigned int m, unsigned int n) {
 unsigned int mcd(unsigned int m, unsigned int n) {
   unsigned int r0 = m;
   unsigned int r1 = n;
-
+  unsigned int aux;
   while (r1 != 0) {
-    unsigned int aux = r0;
+    aux = r0;
     r0 = r1;
     r1 = aux % r1;
   }
@@ -26,12 +26,58 @@ unsigned int mcd(unsigned int m, unsigned int n) {
 /*
 unsigned int mcd(unsigned int m, unsigned int n)
 
-Funcion no hoja
+-- Funcion que recibe dos parametros, devuleve un valor y es una funicion no
+hoja
+-- gp no se necesita ya que no hay variables globales pero hay que tenerlo
+
+#define SS 24 //stack size multiplo de 8
+//SRA
+#define O_FP 20
+#define O_GP 16
+
+//LTA  4*4 = 16 bytes multiplo de 8
+#define O_A0 12
+#define O_A1 8
+#define O_R0 4
+#define O_R1 0
+
+.tex
+.aling 2
+.global mcd
+.ent mcd
+
+mcd:
+
+  subu sp,sp,SS
+  sw fp, O_FP(sp)
+  sw gp, O_GP(gp)
+  move fp, sp
+
+  sw a0, O_A0(fp)
+  sw a1, O_A1(fp)
+
+  lw t0, O_A0(fp)
+  sw t0, O_R0(fp)
+
+  lw t0, O_A1(fp)
+  sw t0, O_R1(fp)
+
+while:
 
 
+end_while:
 
 
+retur:
 
+  li v0,0
+
+  lw fp, O_FP(sp)
+  lw gp, O_GP(sp)
+  addiu sp,sp,SS
+  jr ra
+
+.end mcd
 */
 
 unsigned int mcm(unsigned int m, unsigned int n) {
