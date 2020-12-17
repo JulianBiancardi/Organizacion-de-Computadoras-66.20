27c7c23c5af98489a4ebf70cbe5bd71b42f1275c
JulianBiancardi <JulianBiancardi@github.com>
2020-12-16 21:20:40 -0300

    Add the write-block function

--- TP2/src/main/main.c
+++ TP2/src/main/main.c
@@ -4,9 +4,6 @@
 #include "cache.h"
 #include "constantsTP2.h"
 
-extern unsigned int mcd(unsigned int, unsigned int);
-extern unsigned int mcm(unsigned int, unsigned int);
-
 int main(int argc, char** argv) {
   file_reader_t file_reader;
   if (get_arguments(argc, argv, &file_reader, &cachesize, &ways, &blocksize) !=
