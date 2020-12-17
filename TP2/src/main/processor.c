2333d488cae12eee29265657d5b8b0e0b26754a5
Santiago-Czop <czopsantiago@gmail.com>
2020-12-17 18:23:50 -0300

    [ADD] - Added verification of address and value

--- TP2/src/main/processor.c
+++ TP2/src/main/processor.c
@@ -10,6 +10,8 @@
 #define HIT "HIT"
 #define MISS "MISS"
 
+bool static is_valid_address(int address);
+bool static is_byte(unsigned int value);
 void static invalid_instruction(char* instr, size_t instr_len,
                                 file_writer_t* file_writer);
 
@@ -41,6 +43,11 @@ void init() {
 
 void read(char* instr, file_writer_t* file_writer) {
   int address = (int)strtol(instr, (char**)NULL, 10);
+  if (!is_valid_address(address)) {
+    file_writer_print(file_writer, "Invalid Address: %d", address);
+    return;
+  }
+
   unsigned char byte = cache_read_byte(address);
 
   char* hit_or_miss = cache_hit() ? HIT : MISS;
@@ -51,11 +58,21 @@ void read(char* instr, file_writer_t* file_writer) {
 void write(char* instr, file_writer_t* file_writer) {
   char* next;
   int address = (int)strtol(instr, &next, 10);
+  if (!is_valid_address(address)) {
+    file_writer_print(file_writer, "Invalid Address: %d", address);
+    return;
+  }
+
   // Skip comma and space
   unsigned int value = (unsigned int)strtoul(next + 2, (char**)NULL, 10);
-  cache_write_byte(address, value);
+  if (!is_byte(value)) {
+    file_writer_print(file_writer, "Invalid Value (not a byte): %d\n", value);
+    return;
+  }
 
+  cache_write_byte(address, value);
   char* hit_or_miss = cache_hit() ? HIT : MISS;
+
   file_writer_print(file_writer, "WRITE %02hhX -> %d - %s\n", value, address,
                     hit_or_miss);
 }
@@ -67,4 +84,19 @@ void missrate(file_writer_t* file_writer) {
 void invalid_instruction(char* instr, size_t instr_len,
                          file_writer_t* file_writer) {
   file_writer_print(file_writer, "Invalid Instruction: %s\n", instr);
+}
+
+bool static is_valid_address(int address) {
+  if (address < 0 || address > 65535) {
+    return false;
+  }
+  return true;
+}
+
+bool static is_byte(unsigned int value) {
+  value = value >> 8;
+  if (value > 0) {
+    return false;
+  }
+  return true;
 }
