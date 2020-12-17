5478d119841800f72d6f978c2b2db6bf2ae84877
Santiago-Czop <czopsantiago@gmail.com>
2020-12-17 00:47:05 -0300

    [ADD] - Added Processor code

---
+++ TP2/src/main/processor.c
@@ -0,0 +1,45 @@
+#include "processor.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cache.h"
+
+void process(char* line, size_t line_len) {
+  char* ptr = strchr(line, ' ');
+  int chars_to_cmp = line_len;
+  if (ptr != NULL) {
+    chars_to_cmp = ptr - line;
+  }
+
+  if (strncmp(line, "init", chars_to_cmp) == 0) {
+    init();
+  } else if (strncmp(line, "MR", chars_to_cmp) == 0) {
+    missrate();
+  } else if (strncmp(line, "R", chars_to_cmp) == 0) {
+    read(ptr);
+  } else if (strncmp(line, "W", chars_to_cmp) == 0) {
+    write(ptr);
+  } else {
+    error();
+  }
+}
+
+void init() { cache_init(NULL); }
+
+void read(char* instr) {
+  int address = (int)strtol(instr, (char**)NULL, 10);
+  unsigned char byte = cache_read_byte(NULL, address);
+}
+void write(char* instr) {
+  char* next;
+  int address = (int)strtol(instr, next, 10);
+  // Skip comma and space
+  unsigned int value = (unsigned int)strtoul(next + 2, (char**)NULL, 10);
+  cache_write_byte(NULL, address, value);
+}
+
+void missrate() { cache_get_miss_rate(NULL); }
+
+void error() {}
--- TP2/src/main/processor.h
+++ TP2/src/main/processor.h
@@ -5,6 +5,11 @@ typedef struct processor {
   int FIXME_TODO_DELETE;
 } processor_t;
 
-int process(char* line, size_t line_len);
+void process(char* line, size_t line_len);
+
+void init();
+void read(char* address);
+void write(char* address);
+void missrate();
 
 #endif
